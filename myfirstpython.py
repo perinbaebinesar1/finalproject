c6ec8da0dc807a1892e0f8b8cd7842ad27275075
perinbaebinesar1 <74224389+perinbaebinesar1@users.noreply.github.com>
2021-03-21 15:15:26 +0530

    Update myfirstpython.py

--- myfirstpython.py
+++ myfirstpython.py
@@ -4,9 +4,13 @@ int c=30
 if (a>b)
   {
     if(a>c)
+    {
     print("A is greater than both B and C")
-    elseif(b>c)
+    }
+   elseif(b>c)
+    {
     print("B is greater than both A and C")
-  }
+    }
   else
       print("Cis greater than both A and B")
+  }
