bc736164a4b67c4600bced425c641d73db7e2fe7
perinbaebinesar1 <74224389+perinbaebinesar1@users.noreply.github.com>
2021-03-21 15:12:06 +0530

    Create myfirstpython.py
    this program tells basic concepts of if and if else statement

---
+++ myfirstpython.py
@@ -0,0 +1,12 @@
+int a= 10
+int b=20
+int c=30
+if (a>b)
+  {
+    if(a>c)
+    print("A is greater than both B and C")
+    elseif(b>c)
+    print("B is greater than both A and C")
+  }
+  else
+      print("Cis greater than both A and B")
